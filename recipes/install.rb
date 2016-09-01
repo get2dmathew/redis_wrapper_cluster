91f2f99b7c6f44689ae94967d74ec3bc2ae14a9c
get2dmathew <get2dmathew@gmail.com>
2016-09-01 15:50:13 -0700

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -8,7 +8,8 @@
 include_recipe 'redisio::_install_prereqs'
 include_recipe 'build-essential::default'
 
-
+#TODO Move to standard provider install 
+#TODO move common variables to attributes -opt/redis-3.0.3 etc
 script "install_redis-server" do
   not_if {File.exists?('/opt/redis-3.0.3')}
   interpreter "bash"
@@ -22,7 +23,7 @@ script "install_redis-server" do
     make install
   EOH
 end
-
+# Template with base details for a cluster
 template '/opt/redis-3.0.3/redis.conf' do
   source 'redis.conf.erb'
   owner 'root'
@@ -31,6 +32,7 @@ template '/opt/redis-3.0.3/redis.conf' do
 
 end
 
+# Template for redis restart
 template '/etc/init.d/redis-server' do
   source 'redis-server.erb'
   owner 'root'
@@ -42,8 +44,11 @@ service "redis-server" do
   action :start
 end
 
+# Tag all ndoes as redis - used for  node query later  to look for any disconnected nodes etc
 tag('redis')
 
+# If Node Name has odd number's it will install master redis , else it will install slave redis
+# eg : redis-1 / redis-1.dc1.company.com Tag node accordingly to be used by node search later .
 if node['fqdn'].gsub(/[^\d]/, '')[0].to_i.odd?
   tag('redis_master')
 else
